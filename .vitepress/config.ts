0a95a83a600421d9bfa19758a036d3ddccbd7b41
EN Chu <chuiantw1212@gmail.com>
2026-03-20 07:34:45 +0800

    fix

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   title: "礪石 IFA 工具箱",
   description: "獨立理財顧問的隨身決策終端。礪石工具箱整合退休計算機與多項財務模擬組件，深度優化手機操作感官，拒絕冗餘干擾。將複雜的生命規劃轉化為觸手可及的精確數據，讓專業建議在指尖即刻產出。",
   head: [
-    ['link', { rel: 'icon', href: 'favicon.ico' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
   ],
   appearance: 'dark',
   lang: 'zh-TW',
