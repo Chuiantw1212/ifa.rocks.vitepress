dd368f7c9d1f8a8f5a57bf54984b2574d4f84738
EN Chu <chuiantw1212@gmail.com>
2026-04-01 16:19:49 +0800

    wip

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -6,9 +6,9 @@ import { useDynamicSidebar } from '@/composables/useDynamicSidebar'
 import { useAgent } from '@/composables/useAgent'
 import LoginModule from '@/components/LoginModule.vue'
 import LineBrowserGuard from '@/components/LineBrowserGuard.vue'
+import HeroImage from './components/HeroImage.vue'
 
 const { Layout } = DefaultTheme
-const { frontmatter } = useData()
 
 // --- Layout Hooks ---
 // 監聽 Agent (顧問) 狀態
@@ -39,43 +39,10 @@ onMounted(() => {
       <LoginModule />
     </template>
     <template #home-hero-image>
-      <img
-        v-if="frontmatter.hero && frontmatter.hero.image"
-        class="custom-hero-image"
-        :src="withBase(frontmatter.hero.image.src)"
-        :alt="frontmatter.hero.image.alt"
-        fetchpriority="high"
-      />
+      <HeroImage />
     </template>
   </Layout>
 </template>
 
 <style scoped>
-.custom-hero-image {
-  /* 模仿 VitePress 預設主題的置中與大小設定 */
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  /* Mobile First: 預設大小 (手機) */
-  max-width: 192px;
-  max-height: 192px;
-}
-
-/* 平板 */
-@media (min-width: 640px) {
-  .custom-hero-image {
-    max-width: 256px;
-    max-height: 256px;
-  }
-}
-
-/* 桌面 */
-@media (min-width: 960px) {
-  .custom-hero-image {
-    max-width: 320px;
-    max-height: 320px;
-  }
-}
 </style>
